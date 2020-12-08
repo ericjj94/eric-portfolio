f01820a2250f81884966b435532d5d6b7462e4eb
unknown <eric.joy@mail.valuecoders.com>
2020-12-08 14:19:46 +0530

    Created utils, rendered skills in parts

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -1,62 +1,50 @@
 import React from 'react';
-
-const skillsArray = [
-    {
-        id: 1,
-        name:'Javascript',
-        rating: 4
-    },{
-        id: 2,
-        name:'HTML/CSS',
-        rating: 3.5
-    },{
-        id: 3,
-        name:'ReactJS/Redux',
-        rating: 4
-    },
-    {
-        id: 4,
-        name:'Enzyme,Mocha And Chai',
-        rating: 3.5
-    },
-    {
-        id: 4,
-        name:'React Native',
-        rating: 3
-    },{
-        id: 5,
-        name:'AngularJS',
-        rating: 3
-    },
-    {
-        id: 6,
-        name:'Git',
-        rating: 4
-    }
-]
+import skillsArray from '../../utils/skills';
 
 const Skills = () => {
-    return (
-        <div className="container">
-            <div className="row">
-                <div className="col-md-12">
-                <h2>Skills</h2>
-                <div style={{    
-                    display: 'flex',
-                    flexDirection: 'column'
-                }}>
-                {skillsArray.map((skills)=> (
+
+    function renderSkills(parts){
+        return parts && parts.map((skills)=> (
+            <li style={{ listStyleType: 'none' }}>
                 <React.Fragment>
                     <div className="col-md-6">
                         <label>{skills.name}</label>
                     </div>
                     <div className="col-md-6">
-                    <div class="w3-light-grey" key={skills.id} style={{backgroundColor:'#CCCCCC', borderRadius: '5px'}}>
-                        <div class="w3-grey" style={{height:'50px',width:`${skills.rating*20}%` , backgroundColor: '#313131', borderRadius: '5px' }}></div>
+                    <div class="light-grey" key={skills.id} style={{ backgroundColor:'#CCCCCC'}}>
+                        <div class="grey" style={{height:'50px',width:`${skills.rating*20}%` , backgroundColor: '#313131' }}></div>
                     </div><br /><br />
                 </div>
                 </React.Fragment>
-                ))}
+            </li>
+        )
+        );
+    }
+
+    function splitSkillsArray() {
+        const skills = [];
+        const sep = Math.ceil(skillsArray.length / 2);
+        const firstPart = skillsArray.slice(0, sep);
+        const secondPart = skillsArray.slice(sep, skillsArray.length);
+        skills.push(
+            <ul className="col-xs-6" key="skills-1">
+                {renderSkills(firstPart)}
+            </ul>
+        );
+        skills.push(
+            <ul className="col-xs-6" key="skills-2">
+                {renderSkills(secondPart)}
+            </ul>
+        );
+        return skills;
+      }
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="col-md-12">
+                <h2>Skills</h2>
+                <div>
+                    {splitSkillsArray()}
                 </div>
                 </div>
             </div>
---
+++ src/utils/skills.js
@@ -0,0 +1,49 @@
+const skills =  [
+    {
+        id: 1,
+        name:'Javascript',
+        rating: 4
+    },
+    {
+        id: 4,
+        name:'ReactJS/Redux',
+        rating: 4
+    },
+    {
+        id: 2,
+        name:'HTML/CSS',
+        rating: 3.5
+    },{
+        id: 3,
+        name:'React Native',
+        rating: 4
+    },
+    {
+        id: 4,
+        name:'Enzyme,Mocha And Chai',
+        rating: 3.5
+    },
+    
+    {
+        id: 8,
+        name: 'Node',
+        rating: 3.5
+    },
+    {
+        id: 5,
+        name:'AngularJS',
+        rating: 3
+    },
+    {
+        id: 6,
+        name:'Git',
+        rating: 4
+    },
+    {
+        id: 7,
+        name:'NightwatchJS',
+        rating: 3
+    }
+
+]
+module.exports = skills;
