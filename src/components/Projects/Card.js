f713f33c692f93a44098c61dcf318269ed35460a
Eric Jose <eric.joy@mail.valuecoders.com>
2021-06-02 21:48:11 +0530

    Added sections to the project

--- src/components/Projects/Card.js
+++ src/components/Projects/Card.js
@@ -1,20 +1,30 @@
 import React from "react";
 
-const Card = ({project}) => {
+const Card = ({ project }) => {
   return (
-      <div className="col-md-4">
-    <div className="card">
-      {/* <img src="" className="card-img-top" alt="Project image" /> */}
-      <div className="card-body">
-        <h5 className="card-title" style={{fontSize: '16px', fontStyle:'italic' }}>{project.name}</h5>
-        <p className="card-text">{project.description}
-        </p>
-        {project.showLink ? <a href={project.url || '#'} className="btn btn-primary">
-          Visit Website
-        </a>: <a className="btn btn-primary">Visit Github Profile</a>}
-        
+    <div className='col-md-4'>
+      <div
+        className='card'
+        style={{ border: "1px solid black", borderRadius: "5px" }}
+      >
+        {/* <img src="" className="card-img-top" alt="Project image" /> */}
+        <div className='card-body' style={{ padding: "5px" }}>
+          <h5
+            className='card-title'
+            style={{ fontSize: "16px", fontStyle: "italic" }}
+          >
+            {project.name}
+          </h5>
+          <p className='card-text'>{project.description}</p>
+          {/* {project.showLink ? (
+            <a href={project.url || "#"} className="btn btn-primary">
+              Visit Website
+            </a>
+          ) : (
+            <a className="btn btn-primary">Visit Github Profile</a>
+          )} */}
+        </div>
       </div>
-    </div>
     </div>
   );
 };
--- src/components/Projects/projectsData.js
+++ src/components/Projects/projectsData.js
@@ -1,43 +1,61 @@
-const projectsData = [{
-    name: 'Cofman.com',
-    url: 'http://www.cofman.com',
-    description: 'Introduced React to the legacy application of Cofman. Created React components that are unit tested with enzyme,mocha and chai. Removed the existing jquery and implemented it in React',
-    showLink: true
-},{
-    name:'Resume Builder',
-    url: 'https://github.com/ericjj94/resumebuilder',
-    description: 'Allows a user to create resume using Nodejs. It uses the npm package "html-pdf"',
-    showLink: true
-},{
-    name: 'Hey Travelista',
-    url: 'https://www.facebook.com/heytravelista/',
-    description: 'HeyTravelista is a hotel booking website based out in Hong Kong and is developed in ReactJS. Allows the users to browse to hotels and book the hotels on selected dates.',
-    showLink: true
-},
-{
-    name: 'Tekion Service Menu Admin',
-    url: '',
-    description: 'Created an admin panel for Tekion Corp that allows the admin to add or modify the service details of the vehicles that are currently being used in the US.This would allow the admin to make quick changes in the electric cars updates',
-    showLink: false
-},
-{
-    name: 'PizzaMania',
-    url: '',
-    description: 'A pizza delivery website created for the users in Delhi/NCR region. The app uses geoLocation and is developed in ReactJS',
-    showLink: false
-},
-{
-    name: 'Mono Porto',
-    url: '',
-    platform: 'Mobile- ReactNative',
-    description: 'A parcel delivery app created in React Native that allows the user to send/track parcels from one place to another',
-    showLink: true
-},
-{
-    name: 'Mono Porto Admin Panel',
-    url: '',
-    platform: 'Web',
-    description: 'An admin panel that keeps track of the customers, delivery boys, generates reports and manages transactions',
-    showLink: false
-}]
+const projectsData = [
+  {
+    name: "Cofman.com",
+    url: "http://www.cofman.com",
+    description:
+      "Introduced React to the legacy application of Cofman. Created React components that are unit tested with enzyme,mocha and chai. Removed the existing jquery and implemented it in React",
+    showLink: true,
+  },
+  {
+    name: "SpaceX Launch Application",
+    url: "https://github.com/ericjj94",
+    description:
+      "SpaceX Launch Application is a server side rendered React application that renders the Spacex Launch(s) in the past years. It uses Express to render the HTML.",
+    showLink: false,
+  },
+  {
+    name: "Resume Builder",
+    url: "https://github.com/ericjj94/resumebuilder",
+    description:
+      'Allows a user to create resume using Nodejs. It uses the npm package "html-pdf"',
+    showLink: true,
+  },
+  {
+    name: "Hey Travelista",
+    url: "https://www.facebook.com/heytravelista/",
+    description:
+      "HeyTravelista is a hotel booking website based out in Hong Kong and is developed in ReactJS. Allows the users to browse to hotels and book the hotels on selected dates.",
+    showLink: true,
+  },
+  {
+    name: "Tekion Service Menu Admin",
+    url: "",
+    description:
+      "Created an admin panel for Tekion Corp that allows the admin to add or modify the service details of the vehicles that are currently being used in the US.This would allow the admin to make quick changes in the electric cars",
+    showLink: false,
+  },
+  {
+    name: "PizzaMania",
+    url: "",
+    description:
+      "A pizza delivery website created for the users in Delhi/NCR region. The app uses geoLocation and is developed in ReactJS",
+    showLink: false,
+  },
+  {
+    name: "Mono Porto",
+    url: "",
+    platform: "Mobile- ReactNative",
+    description:
+      "A parcel delivery app created in React Native that allows the user to send/track parcels from one place to another",
+    showLink: true,
+  },
+  {
+    name: "Mono Porto Admin Panel",
+    url: "",
+    platform: "Web",
+    description:
+      "An admin panel that keeps track of the customers, delivery boys, generates reports and manages transactions",
+    showLink: false,
+  },
+];
 export default projectsData;
